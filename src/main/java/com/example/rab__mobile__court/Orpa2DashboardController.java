89d0e008d27510388282f4e59de2f5d48e63598d
protik2230760 <protiksaidur2001@gmail.com>
2024-12-11 17:35:11 +0600

    //

--- src/main/java/com/example/rab__mobile__court/Orpa2DashboardController.java
+++ src/main/java/com/example/rab__mobile__court/Orpa2DashboardController.java
@@ -2,46 +2,14 @@ package com.example.rab__mobile__court;
 
 import javafx.event.ActionEvent;
 
-public class Orpa1DashboardController
+public class Orpa2DashboardController
 {
     @javafx.fxml.FXML
     public void initialize() {
     }
 
-    @Deprecated
-    public void assignSurveyMissionOnClick(ActionEvent actionEvent) {
-    }
-
-    @Deprecated
-    public void viewSurveyReportOnClick(ActionEvent actionEvent) {
-    }
-
     @javafx.fxml.FXML
     public void signOutOA(ActionEvent actionEvent) {
-    }
-
-    @Deprecated
-    public void EmergencySupportOA(ActionEvent actionEvent) {
-    }
-
-    @Deprecated
-    public void generateCaseReportOnClick(ActionEvent actionEvent) {
-    }
-
-    @Deprecated
-    public void ApproveSurveyRequestOnClick(ActionEvent actionEvent) {
-    }
-
-    @Deprecated
-    public void scheduleMeetingOnClick(ActionEvent actionEvent) {
-    }
-
-    @Deprecated
-    public void makeChartOfCaseOnClick(ActionEvent actionEvent) {
-    }
-
-    @Deprecated
-    public void viewSurveyChartOnAction(ActionEvent actionEvent) {
     }
 
     @javafx.fxml.FXML
